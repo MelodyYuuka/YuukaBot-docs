7c615e14b6b113526c9ac6a905e965600b41372e
Melody_Yuuka <a876987@126.com>
2024-06-01 23:10:45 +0800

    恢复统计

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { removeHtmlExtensionPlugin } from 'vuepress-plugin-remove-html-extension'
+import { removeHtmlExtensionPlugin } from 'vuepress-plugin-remove-html-extension';
+import { baiduAnalyticsPlugin } from '@vuepress/plugin-baidu-analytics';
 import { getDirname, path } from 'vuepress/utils';
 
 const __dirname = getDirname(import.meta.url);
@@ -19,6 +20,9 @@ export default defineUserConfig({
   ],
   theme,
   plugins: [
+    baiduAnalyticsPlugin({
+      id: "7a0d3a4e8454cc167b63e7dc1de54488"
+    }),
     removeHtmlExtensionPlugin()
   ],
   clientConfigFile: path.resolve(__dirname, "./client.ts"),
